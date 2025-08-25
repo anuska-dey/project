c50fba1f37c07af11cfe72640b2e389838f4e6b4
Anuska Dey <anuskadey18511@gmail.com>
2025-08-25 08:24:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,37 @@
-import speech_recognition as sr
+import speech_recognition as sr 
 import webbrowser
-import pyttsx3
-
+import pyttsx3 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
-
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-def processCommand(command):
-    command = command.lower()
-    print(f"Command received: {command}")
+def processCommand(c):
+    pass
 
-    if "open google" in command:
-        speak("Opening Google")
-        webbrowser.open("https://www.google.com")
-
-    elif "open youtube" in command:
-        speak("Opening YouTube")
-        webbrowser.open("https://www.youtube.com")
-
-    elif "stop" in command:
-        speak("Goodbye!")
-        exit()
-
-    else:
-        speak("Sorry, I didn’t understand that.")
-
-if __name__ == "__main__":
+if _name_ == "_main_" :
     speak("Initializing")
     while True:
+        #listen the wake word
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+       
+            
+        print("recognizing")
         try:
             with sr.Microphone() as source:
-                print("Listening for wake word...")
-                audio = recognizer.listen(source, timeout=5, phrase_time_limit=10)
-            word = recognizer.recognize_google(audio)
-
-            if word.lower() == "jarvis":
-                speak("Yes, I am listening")  # finish speaking first
-
-                # only after speaking, start listening
+                print("Listening..")
+                audio = r.listen(source,timeout=2,phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                speak("yes")
+                #listen for command
                 with sr.Microphone() as source:
                     print("Jarvis Active..")
-                    audio = recognizer.listen(source, timeout=5, phrase_time_limit=10)
-                    command = recognizer.recognize_google(audio)
-                    processCommand(command)
-
-
-        except sr.UnknownValueError:
-            print("Sorry, I could not understand.")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+                    
+                    processCommand()
         except Exception as e:
-            print(f"Error: {e}")
+            print("Error; {0}".format(e))
